8af4a95c5d4c0397dd4f07894baf9652e3a4b899
HippopoStar <leocabanes@wanadoo.fr>
2021-04-30 03:46:23 +0200

    Des avancees interessantes dans EI2ANGNOD

--- EI2ANGNOD/compose_EI2ANGNOD/backend/run_backend_container.sh
+++ EI2ANGNOD/compose_EI2ANGNOD/backend/run_backend_container.sh
@@ -2,9 +2,23 @@
 
 IMAGE_NAME='ei2angnod_backend:latest'
 CONTAINER_NAME='ei2angnod_backend_container'
+DATABASE_CONTAINER_NAME='ei2angnod_database_container'
+
+NODE_SERVER_FILENAME='OnlineSalesServer.js'
+NODE_HTTPS_SERVER_FILENAME='OnlineSalesHttpsServer.js'
 
 docker stop "${CONTAINER_NAME}"
 docker image rm "${IMAGE_NAME}"
 docker build --tag "${IMAGE_NAME}" ./
-docker run --name "${CONTAINER_NAME}" --interactive --tty --publish 8888:8888 --rm "${IMAGE_NAME}" bash
+echo "node ${NODE_SERVER_FILENAME} &"
+echo "node ${NODE_HTTPS_SERVER_FILENAME} &"
+echo 'curl -X GET localhost:8888/Products/selectors'
+docker run \
+	--name "${CONTAINER_NAME}" \
+	--interactive \
+	--tty \
+	--publish 8888:8888 \
+	--link "${DATABASE_CONTAINER_NAME}" \
+	--rm \
+	"${IMAGE_NAME}" bash
 
--- EI2ANGNOD/compose_EI2ANGNOD/database/run_database_container.sh
+++ EI2ANGNOD/compose_EI2ANGNOD/database/run_database_container.sh
@@ -10,11 +10,14 @@ CONTAINER_NAME='ei2angnod_database_container'
 show_collection () {
 	if [ ${#} -eq 2 ] ; then # [ expression ] : '[' etant un nom fonction, les espaces autour sont importants
 		echo "Database '${1}' - Collection '${2}':"
-		docker exec "${CONTAINER_NAME}" sh -c "exec mongoexport --pretty --db '${1}' --collection '${2}'"
+		docker exec \
+			"${CONTAINER_NAME}" sh -c "exec mongoexport --uri=\"mongodb://root:example@127.0.0.1:27017/${1}?authSource=admin\" --pretty --collection '${2}'"
+#			"${CONTAINER_NAME}" sh -c "exec mongoexport --username='root' --password='example' --pretty --db '${1}' --collection '${2}'"
+
 	fi
 }
 
-if [ ${#} -ne 1 -o \( 'xrun' != "x${1}" -a 'xshow' != "x${1}" \) ] ; then
+if [ ${#} -ne 1 -o \( 'xrun' != "x${1}" -a 'xshow' != "x${1}" \) ] ; then # Les parentheses doivent etre echappees pour que cela fonctionne
 	usage # pas de parentheses quand on appelle la fonction
 else
 	if [ 'xrun' = "x${1}" ] ; then
@@ -23,8 +26,22 @@ else
 		docker image rm "${IMAGE_NAME}"
 		docker build --tag "${IMAGE_NAME}" ./
 
-		docker run --name "${CONTAINER_NAME}" --detach --publish 27017:27017 --rm "${IMAGE_NAME}"
-		#docker exec --interactive --tty "${CONTAINER_NAME}" bash
+		docker run \
+			--name "${CONTAINER_NAME}" \
+			--interactive \
+			--tty \
+			--env MONGO_INITDB_ROOT_USERNAME=root \
+			--env MONGO_INITDB_ROOT_PASSWORD=example \
+			--publish 27017:27017 \
+			--rm \
+			"${IMAGE_NAME}"
+
+#			--detach \
+#			--interactive \
+#			--tty \
+
+#			--env MONGO_INITDB_ROOT_USERNAME=root \
+#			--env MONGO_INITDB_ROOT_PASSWORD=example \
 
 	elif [ 'xshow' = "x${1}" ] ; then
 
